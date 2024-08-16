fbff419bd97590eb7157236127a829764f50e5b0
KiRiSv <kimrikters@gmail.com>
2024-08-16 19:05:49 -0400

    added raycasts

--- Prey.cs
+++ Prey.cs
@@ -4,23 +4,18 @@ using static MLtest;
 
 public partial class Prey : Creature
 {
-	static int layerSize = 10;
-	static int inputSize = 10;
-	static int outSize = 4;
-	static int speed = 15;
-	static int mutatetionChance = 10;
-	double[,] hiddenLayer = new double[layerSize,inputSize];
-	double[,] output = new double[outSize,layerSize];
+	public override float fov {get;set;} = 1.75F;
+	static PackedScene preyScene = GD.Load<PackedScene>("res://prey.tscn");
 	
-	private void _on_body_entered(Node body)
-	{
+	
+	private void _on_body_entered(Node body){
 		//GD.Print(body.GetType());
 		if(body is Food){
 			body.CallDeferred("queue_free");
-			PackedScene preyScene = GD.Load<PackedScene>("res://prey.tscn");
 			Prey preyInstance = (Prey) preyScene.Instantiate();
 			preyInstance.clone(this);
-			AddChild(preyInstance);
+			preyInstance.Position = Position;
+			GetParent().AddChild(preyInstance);
 		}
 	}
 }
